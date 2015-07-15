7f15da57c29055bee8eea094dc91cb7081bf300c
Daria Zolotko <nalune@gmail.com>
2015-07-15 11:21:24 -0700

    added tweens and popups

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,5 +1,7 @@
 $(function() {
   var topoffset = 43;
+
+  //window height
   var wheight = $(window).height(); //get height of the window
 
   $('.fullheight').css('height', wheight);
@@ -10,7 +12,7 @@ $(function() {
   }) //on resize
 
 
-
+// Animated Scrolling
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -20,11 +22,12 @@ $(function() {
           scrollTop: target.offset().top-topoffset
         }, 1000);
         return false;
-      }
-    }
-  });
+      } // target.length
+    } //location hostname
+  }); //on click
 
-$(window).scroll(function() {
+  //highlight navigation
+  $(window).scroll(function() {
     var windowpos = $(window).scrollTop() + topoffset;
     $('nav li a').removeClass('active');
 
@@ -57,6 +60,7 @@ $(window).scroll(function() {
 
 
 
+
   //set up ScrollMagic
   var controller = new ScrollMagic({
     globalSceneOptions: {
@@ -64,12 +68,104 @@ $(window).scroll(function() {
     }
   });
 
-
+  //pin the navigation
   var pin = new ScrollScene({
     triggerElement: '#nav',
   }).setPin('#nav').addTo(controller);
 
 
+  //room animations
+  var roomOrigin = {
+    bottom: -700,
+    opacity: 0,
+    scale: 0
+  }
+
+  var roomDest = {
+    repeat: 1,
+    yoyo: true,
+    bottom: 0,
+    opacity: 1,
+    scale: 1,
+    ease: Back.easeOut
+  }
+
+  var roomtween = TweenMax.staggerFromTo(
+    "#westminster .content",
+    1, roomOrigin, roomDest);
+
+  var pin = new ScrollScene({
+    triggerElement: '#westminster',
+    offset: -topoffset,
+    duration: 500
+  }).setPin('#westminster')
+    .setTween(roomtween)
+    .addTo(controller)
+
+
+  var roomtween = TweenMax.staggerFromTo(
+    "#oxford .content",
+    1, roomOrigin, roomDest);
+
+  var pin = new ScrollScene({
+    triggerElement: '#oxford',
+    offset: -topoffset,
+    duration: 500
+  }).setPin('#oxford')
+    .setTween(roomtween)
+    .addTo(controller)
+
+  var roomtween = TweenMax.staggerFromTo(
+    "#victoria .content",
+    1, roomOrigin, roomDest);
+
+  var pin = new ScrollScene({
+    triggerElement: '#victoria',
+    offset: -topoffset,
+    duration: 500
+  }).setPin('#victoria')
+    .setTween(roomtween)
+    .addTo(controller)
+
+  var roomtween = TweenMax.staggerFromTo(
+    "#manchester .content",
+    1, roomOrigin, roomDest);
+
+  var pin = new ScrollScene({
+    triggerElement: '#manchester',
+    offset: -topoffset,
+    duration: 500
+  }).setPin('#manchester')
+    .setTween(roomtween)
+    .addTo(controller)
+
+  var roomtween = TweenMax.staggerFromTo(
+    "#piccadilly .content",
+    1, roomOrigin, roomDest);
+
+  var pin = new ScrollScene({
+    triggerElement: '#piccadilly',
+    offset: -topoffset,
+    duration: 500
+  }).setPin('#piccadilly')
+    .setTween(roomtween)
+    .addTo(controller)
+
+
+  var roomtween = TweenMax.staggerFromTo(
+    "#cambridge .content",
+    1, roomOrigin, roomDest);
+
+  var pin = new ScrollScene({
+    triggerElement: '#cambridge',
+    offset: -topoffset,
+    duration: 500
+  }).setPin('#cambridge')
+    .setTween(roomtween)
+    .addTo(controller)
+
+
+  //atractions animation
   var attractionstween = TweenMax.staggerFromTo('#attractions article', 1, { opacity: 0, scale: 0 },
       {delay: 1, opacity: 1, scale: 1,
         ease: Back.easeOut});
